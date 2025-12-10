691fc429b39fca1d44ed71af996aa63d0fd1eb0b
Priyanshu Paul <priyanshupaul90@gmail.com>
2025-12-10 22:37:33 +0530

    Remove headers configuration from next.config.ts
    Removed custom headers for all routes.

--- next.config.ts
+++ next.config.ts
@@ -15,20 +15,7 @@ const nextConfig: NextConfig = {
   },
 
  async headers() {
-    return [
-      {
-        // Apply to all routes
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-        ],
+    return [ ],
       },
     ];
   },
