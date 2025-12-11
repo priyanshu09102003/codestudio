417c29568f86f9e72e70bff659805433b26eafaf
Priyanshu Paul <priyanshupaul90@gmail.com>
2025-12-11 08:55:39 +0530

    Update template paths for starter projects

--- lib/template.ts
+++ lib/template.ts
@@ -1,8 +1,8 @@
 export const templatePaths = {
-    REACT: 'starters/react',
-    NEXTJS: 'starters/nextjs-new',
-    EXPRESS: 'starters/express-simple',
-    VUE: 'starters/vue',
-    HONO: 'starters/hono-nodejs-starter',
-    ANGULAR: 'starters/angular',
-  };
+    REACT: 'public/starters/gsap-react',
+    NEXTJS: 'public/starters/gsap-next',
+    EXPRESS: 'public/starters/express-simple',
+    VUE: 'public/starters/gsap-vue',
+    HONO: 'public/starters/hono-nodejs-starter',
+    ANGULAR: 'public/starters/angular',
+};
